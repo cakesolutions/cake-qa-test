7e57d5cadf16d720afd0d0a99cff62db014dbf08
alexlashford <alexl@cakesolutions.net>
2016-09-20 12:41:57 +0100

    App generated

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('cakeQaTest')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
