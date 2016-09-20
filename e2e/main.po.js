dd84fde5779633e774260d8baca4dfda3ed61c4e
alexlashford <alexl@cakesolutions.net>
2016-09-20 13:50:30 +0100

    second commit for first page

--- e2e/main.po.js
+++ e2e/main.po.js
@@ -6,10 +6,9 @@
 'use strict';
 
 var MainPage = function() {
-  this.jumbEl = element(by.css('.jumbotron'));
+  this.jumbEl = element(by.css('#header-panel'));
   this.h1El = this.jumbEl.element(by.css('h1'));
   this.imgEl = this.jumbEl.element(by.css('img'));
-  this.thumbnailEls = element(by.css('body')).all(by.repeater('awesomeThing in main.awesomeThings'));
 };
 
 module.exports = new MainPage();
